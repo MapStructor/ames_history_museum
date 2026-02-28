c7d7bd42840090a2087a1537b436a49da49e11d9
Vishapasar <77992836+Vishapasar@users.noreply.github.com>
2026-02-28 18:15:56 +0400

    Update modalinfo.js AHM init
    initial project

--- timeline/js/modalinfo.js
+++ timeline/js/modalinfo.js
@@ -1,22 +1,56 @@
-function getInfoText(modal_header_text, modal_content_html) {
-	$.ajax({
-		url: 'https://encyclopedia.nahc-mapping.org/info-text-export',
-		headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-		type: 'get',
-		dataType: 'json',
-		data: {}
-	}).done(function (data) {
-		if (data.length > 0) {
-			for (let i = 0; i < data.length; i++) {
-				if (data[i].id.length > 0) {
-					modal_header_text[data[i].id] = data[i].title.replace(/&amp;/g, '&');
-					modal_content_html[data[i].id] = data[i].body;
-				}
-			}
-		}
-	}).fail(function (xhr, textStatus) {
-		console.warn("jQuery AJAX request  ERROR !!!");
-		console.log(xhr.responseText);
-		console.log(textStatus);
-	});
-}
+var modal_header_text = [];
+var modal_content_html = [];
+
+modal_header_text["about"] = "ABOUT";
+modal_content_html["about"] = `
+	<p>
+		Welcome to <b>Ames History Museum</b> Timeline
+		<br/><br/>
+		<img style="width: 100%;" src="./icons/AHM.jpg" />
+		<br/><br/>
+		Questions? Contact us at 
+		<a href="mailto: info@ameshistory.org">info@ameshistory.org</a>
+	</p>
+`;
+
+modal_header_text["builds-info-layer"] = "Buildings";
+modal_content_html["builds-info-layer"] = `
+	<p>
+		Buildings layer info
+	</p>
+`;
+
+modal_header_text["roads-info-layer"] = "Roads";
+modal_content_html["roads-info-layer"] = `
+	<p>
+		Roads layer info
+	</p>
+`;
+
+modal_header_text["parcels-info-layer"] = "Parcels";
+modal_content_html["parcels-info-layer"] = `
+	<p>
+		Parcels layer info
+	</p>
+`;
+
+modal_header_text["plss-parcels-info-layer"] = "PLSS Parcels";
+modal_content_html["plss-parcels-info-layer"] = `
+	<p>
+		PLSS Parcels layer info
+	</p>
+`;
+
+modal_header_text["rail-roads-info-layer"] = "Railroads";
+modal_content_html["rail-roads-info-layer"] = `
+	<p>
+		Railroads layer info
+	</p>
+`;
+
+modal_header_text["city-limits-info-layer"] = "City limits";
+modal_content_html["city-limits-info-layer"] = `
+	<p>
+		City limits layer info
+	</p>
+`;
