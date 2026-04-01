547097b8196071058bd1f22c6257a009d903b73b
nittyjee <nittyjee@gmail.com>
2026-04-01 03:52:02 +0000

    Deploying to gh-pages from @ MapStructor/ames_history_museum@6a16c6de8d7b3abd362726946017c420aae4d08e 🚀

--- dev/timeline/js/eventsHandle.js
+++ dev/timeline/js/eventsHandle.js
@@ -1,86 +1,115 @@
-function addEvents() {
-  eventLayers.forEach(layer => {
-    addHoverEvent(beforeMap, { ...layer, layerID: layer.layerID + "-left",  layerIDhigh: layer.layerIDhigh + "-left",  index: layer.index + "-left"  });
-    addHoverEvent(afterMap,  { ...layer, layerID: layer.layerID + "-right", layerIDhigh: layer.layerIDhigh + "-right", index: layer.index + "-right" });
+let hoveredID = new Array();
+let hoverPopUp = new Array();
+
+function setupLayerEvents() {
+  layerEvents.forEach(config => {
+    setupLayerEventForMap(beforeMap, config, "left");
+    setupLayerEventForMap(afterMap,  config, "right");
   });
 }
 
-function addHoverEvent(map, layerConfig) {
-	//console.warn("- add an event -");
-	console.log(layerConfig.index);
-	console.log(layerConfig.layerID);
-	console.log(layerConfig.layerSource);
-	
-	hoveredID[layerConfig.index] = null;
-	
-	hoverPopUp[layerConfig.index] = new mapboxgl.Popup({
-		closeButton: false,
-		closeOnClick: false,
-	});
-	
-	map.on("mouseenter", layerConfig.layerID, function (e) {
+function setupLayerEventForMap(map, config, side) {
+  const layerID = config.id + "-" + side;
+  const index   = config.id + "-" + side;
+
+  // HOVER
+  if (config.hover) {
+    hoveredID[index] = null;
+    hoverPopUp[index] = new mapboxgl.Popup({ closeButton: false, closeOnClick: false });
+
+    map.on("mouseenter", layerID, function (e) {
       map.getCanvas().style.cursor = "pointer";
-      hoverPopUp[layerConfig.index].setLngLat(e.lngLat).addTo(map);
+      hoverPopUp[index].setLngLat(e.lngLat).addTo(map);
     });
 
-    map.on("mousemove", layerConfig.layerID, function (e) {
-	  map.getCanvas().style.cursor = "pointer";
+    map.on("mousemove", layerID, function (e) {
+      map.getCanvas().style.cursor = "pointer";
       if (e.features.length > 0) {
-        if (hoveredID[layerConfig.index]) {
-          map.setFeatureState(
-            {
-              source: layerConfig.layerID,
-              sourceLayer: layerConfig.layerSource,
-              id: hoveredID[layerConfig.index],
-            },
-            { hover: false }
-          );
+        if (hoveredID[index]) {
+          map.setFeatureState({ source: layerID, sourceLayer: config.sourceLayer, id: hoveredID[index] }, { hover: false });
         }
-        hoveredID[layerConfig.index] = e.features[0].id;
-        map.setFeatureState(
-          {
-            source: layerConfig.layerID,
-            sourceLayer: layerConfig.layerSource,
-            id: hoveredID[layerConfig.index],
-          },
-          { hover: true }
-        );
-		
-		//console.log(e.features[0].id);
-		
-        var PopUpHTML = "<div class='" + layerConfig.popupStyle + "'>";
-		var popup_view_list = [];
-		
-			for (let i = 0, l = layerConfig["popupParamsList"].length; i < l; ++i) {
-				if(typeof e.features[0].properties[layerConfig.popupParamsList[i]] !== 'undefined') {
-					popup_view_list.push(e.features[0].properties[layerConfig.popupParamsList[i]]);
-				}	
-			}
+        hoveredID[index] = e.features[0].id;
+        map.setFeatureState({ source: layerID, sourceLayer: config.sourceLayer, id: hoveredID[index] }, { hover: true });
 
-        //BEFORE MAP POP UP CONTENTS
-		if(popup_view_list.length > 0) {
-			PopUpHTML += popup_view_list.join("<br/>") + "</div>"
-			hoverPopUp[layerConfig.index].setLngLat(e.lngLat).setHTML(PopUpHTML);
-		}
-		
+        const popup_view_list = config.hover.params
+          .filter(p => typeof e.features[0].properties[p] !== 'undefined')
+          .map(p => e.features[0].properties[p]);
+
+        if (popup_view_list.length > 0) {
+          const html = "<div class='" + config.hover.popupStyle + "'>" + popup_view_list.join("<br/>") + "</div>";
+          hoverPopUp[index].setLngLat(e.lngLat).setHTML(html);
+        }
       }
     });
 
-    map.on("mouseleave", layerConfig.layerID, function () {
+    map.on("mouseleave", layerID, function () {
       map.getCanvas().style.cursor = "";
-      if (hoveredID[layerConfig.index]) {
-        map.setFeatureState(
-          {
-            source: layerConfig.layerID,
-            sourceLayer: layerConfig.layerSource,
-            id: hoveredID[layerConfig.index],
-          },
-          { hover: false }
-        );
+      if (hoveredID[index]) {
+        map.setFeatureState({ source: layerID, sourceLayer: config.sourceLayer, id: hoveredID[index] }, { hover: false });
       }
-      hoveredID[layerConfig.index] = null;
-      if (hoverPopUp[layerConfig.index].isOpen()) hoverPopUp[layerConfig.index].remove();
+      hoveredID[index] = null;
+      if (hoverPopUp[index].isOpen()) hoverPopUp[index].remove();
     });
-	
+  }
+
+  // CLICK
+  if (config.click) {
+    let viewId = null;
+    const afterPopup  = new mapboxgl.Popup({ closeButton: false, closeOnClick: false, offset: 5 });
+    const beforePopup = new mapboxgl.Popup({ closeButton: false, closeOnClick: false, offset: 5 });
+
+    const highlightLeft  = config.id + "-highlighted-left";
+    const highlightRight = config.id + "-highlighted-right";
+
+    function setHighlight(m, source, featureId, isHovered) {
+      if (featureId == null) return;
+      m.setFeatureState({ source, sourceLayer: config.sourceLayer, id: featureId }, { hover: isHovered });
+    }
+
+    function buildPopupHTML(event) {
+      const val = event.features?.[0]?.properties?.[config.click.prop] ?? "";
+      return "<div class='" + config.click.popupStyle + "'>" + val + "</div>";
+    }
+
+    function closeInfo() {
+      setHighlight(afterMap,  highlightRight, viewId, false);
+      setHighlight(beforeMap, highlightLeft,  viewId, false);
+      viewId = null;
+      if (afterPopup.isOpen())  afterPopup.remove();
+      if (beforePopup.isOpen()) beforePopup.remove();
+    }
+
+    function clickHandle(event) {
+      const clickedId = event.features?.[0]?.id;
+      if (clickedId == null) return;
+      if (viewId === clickedId) { closeInfo(); return; }
+
+      setHighlight(afterMap,  highlightRight, viewId, false);
+      setHighlight(afterMap,  highlightRight, clickedId, true);
+      setHighlight(beforeMap, highlightLeft,  viewId, false);
+      setHighlight(beforeMap, highlightLeft,  clickedId, true);
+      viewId = clickedId;
+
+      const html = buildPopupHTML(event);
+      afterPopup.setLngLat(event.lngLat).setHTML(html);
+      beforePopup.setLngLat(event.lngLat).setHTML(html);
+      if (!afterPopup.isOpen())  afterPopup.addTo(afterMap);
+      if (!beforePopup.isOpen()) beforePopup.addTo(beforeMap);
+    }
+
+    // Only register click on one side to avoid double-firing
+    if (side === "right") {
+      beforeMap.on("click", config.id + "-left",  clickHandle);
+      afterMap.on("click",  config.id + "-right", clickHandle);
+
+      const mainCheckbox  = document.getElementById(config.id);
+      const groupCheckbox = config.groupId ? document.getElementById(config.groupId) : null;
+      if (mainCheckbox)  mainCheckbox.addEventListener("change",  function () { if (!mainCheckbox.checked)  closeInfo(); });
+      if (groupCheckbox) groupCheckbox.addEventListener("change", function () { if (!groupCheckbox.checked) closeInfo(); });
+    }
+  }
 }
 
+function addEvents() {
+  setupLayerEvents();
+}
