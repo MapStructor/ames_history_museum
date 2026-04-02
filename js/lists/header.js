40f1590d133d7d6be15bb41282ea043b16b4fad1
nittyjee <nittyjee@gmail.com>
2026-04-01 23:05:18 -0500

    Created list file for header (logo, text, buttons)

---
+++ js/lists/header.js
@@ -0,0 +1,23 @@
+const siteLogoLink  = "https://ameshistory.org/";
+const siteLogoImage = "./icons/banner_thumbnail.png";
+const siteHeaderText = "Map";
+
+const headerButtons = [
+
+  {
+    label: "ABOUT",
+    type: "modal",
+    id: "about",
+  },
+
+  /*
+  {
+    label: "Visit Site",
+    type: "link",
+    url: "https://ameshistory.org/",
+    newTab: true,   // default true; false opens in same tab
+  },
+  */
+  
+
+];
